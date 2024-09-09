3651b2cad1e1ed12049b2ca2078d2557c764eb0b
TsarBomba <164365832+Juhwan24@users.noreply.github.com>
2024-09-09 15:31:25 +0900

    Add files via upload

---
+++ practice01.js
@@ -0,0 +1,24 @@
+//구구단 실습
+alert("어서온나 친구들아!");
+/*
+let k=confirm("나랑 구구단 구구단 하싈?");
+var str,str_s;
+if (k)
+{
+	var n;
+	n=parseInt(prompt("몇 단까지 출력하나요? "))
+	for(var i=1;i<=n;i++)
+	{
+		str_s=i+"단을 출력합니다.\n";
+		for(var j=1;j<=9;j++)
+		{
+			str=i+" * "+j+" = "+i*j;
+			str_s+=str+"\n";
+		}
+		alert(str_s);
+	}
+	alert("program finished");
+}
+else
+	alert("program finished by user");
+*/
